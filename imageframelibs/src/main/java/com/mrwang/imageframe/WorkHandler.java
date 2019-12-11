ab81a9775f991a363c8c2132abb9bff6d468efa2
yhx13989877447 <810971230@qq.com>
2019-12-11 15:52:44 +0800

    修改bugly上的一个偶现空指针问题 handler执行最后一个msg时，列表中的对象被移除

--- imageframelibs/src/main/java/com/mrwang/imageframe/WorkHandler.java
+++ imageframelibs/src/main/java/com/mrwang/imageframe/WorkHandler.java
@@ -15,72 +15,80 @@ import java.util.List;
  * Time: 下午4:39
  */
 public class WorkHandler extends android.os.HandlerThread {
-  private Handler workHandler = null;
+    private Handler workHandler = null;
 
-  private volatile List<WorkMessageProxy> messageProxyList;
+    private volatile List<WorkMessageProxy> messageProxyList;
 
-  Looper looper = null;
+    Looper looper = null;
 
-  public WorkHandler() {
-    super("WorkHandler", Process.THREAD_PRIORITY_BACKGROUND);
-    start();
-    looper = getLooper();
-    workHandler = new Handler(looper) {
-      @Override
-      public void handleMessage(Message msg) {
-        if (messageProxyList != null) {
-          for (WorkMessageProxy workMessageProxy : messageProxyList) {
-            workMessageProxy.handleMessage(msg);
-          }
+    public WorkHandler() {
+        super("WorkHandler", Process.THREAD_PRIORITY_BACKGROUND);
+        start();
+        looper = getLooper();
+        workHandler = new Handler(looper) {
+            @Override
+            public void handleMessage(Message msg) {
+                //根据bugly这里偶现一个空指针
+                try {
+                    if (null != messageProxyList && messageProxyList.size() > 0) {
+                        for (WorkMessageProxy workMessageProxy : messageProxyList) {
+                            if (null != workMessageProxy) {
+                                workMessageProxy.handleMessage(msg);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e){
+
+                }
+            }
+        };
+    }
+
+    public void quitLooper() {
+        if (null != looper) {
+            looper.quit();
         }
-      }
-    };
-  }
-
-  public void quitLooper(){
-    if(null != looper){
-      looper.quit();
     }
-  }
 
-  public void post(Runnable run) {
-    workHandler.post(run);
-  }
-
-  public void postAtFrontOfQueue(Runnable runnable) {
-    workHandler.postAtFrontOfQueue(runnable);
-  }
-
-  public void postDelayed(Runnable runnable, long delay) {
-    workHandler.postDelayed(runnable, delay);
-  }
-
-  public void postAtTime(Runnable runnable, long time) {
-    workHandler.postAtTime(runnable, time);
-  }
-
-  public void addMessageProxy(WorkMessageProxy proxy) {
-    initMessageProxyList();
-    messageProxyList.add(proxy);
-  }
-
-  public void removeMessageProxy(WorkMessageProxy proxy) {
-    initMessageProxyList();
-    messageProxyList.remove(proxy);
-  }
-
-  private void initMessageProxyList() {
-    if (messageProxyList == null) {
-      messageProxyList = new ArrayList<>();
+    public void post(Runnable run) {
+        workHandler.post(run);
     }
-  }
 
-  public Handler getHandler() {
-    return workHandler;
-  }
+    public void postAtFrontOfQueue(Runnable runnable) {
+        workHandler.postAtFrontOfQueue(runnable);
+    }
 
-  public interface WorkMessageProxy {
-    void handleMessage(Message msg);
-  }
+    public void postDelayed(Runnable runnable, long delay) {
+        workHandler.postDelayed(runnable, delay);
+    }
+
+    public void postAtTime(Runnable runnable, long time) {
+        workHandler.postAtTime(runnable, time);
+    }
+
+    public void addMessageProxy(WorkMessageProxy proxy) {
+        initMessageProxyList();
+        messageProxyList.add(proxy);
+    }
+
+    public void removeMessageProxy(WorkMessageProxy proxy) {
+        initMessageProxyList();
+        messageProxyList.remove(proxy);
+    }
+
+    private void initMessageProxyList() {
+        if (messageProxyList == null) {
+            messageProxyList = new ArrayList<>();
+        }
+    }
+
+    public Handler getHandler() {
+        return workHandler;
+    }
+
+    public interface WorkMessageProxy {
+        void handleMessage(Message msg);
+    }
 
 }
